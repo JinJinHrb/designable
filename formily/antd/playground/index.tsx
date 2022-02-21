b18149ff042be0b7f33f7c1d78fd42f5172c09bf
JinJin_Hrb <fanwang1987@gmail.com>
2022-02-21 17:34:33 +0800

    feat: 微前端改造 doing

---
+++ formily/antd/playground/index.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import '../public-path'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './main'
+
+function render(props) {
+  const { container } = props
+  ReactDOM.render(
+    <App />,
+    container
+      ? container.querySelector('#root')
+      : document.querySelector('#root')
+  )
+}
+
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  render({})
+}
+
+export async function bootstrap() {
+  console.log('[react16] react app bootstraped')
+}
+
+export async function mount(props) {
+  console.log('[react16] props from main framework', props)
+  render(props)
+}
+
+export async function unmount(props) {
+  const { container } = props
+  ReactDOM.unmountComponentAtNode(
+    container
+      ? container.querySelector('#root')
+      : document.querySelector('#root')
+  )
+}
