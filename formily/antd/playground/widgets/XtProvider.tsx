7529909b3b4d76d807fe3bccab641439cb5f5d3b
JinJin_Hrb <fanwang1987@gmail.com>
2022-02-23 21:19:11 +0800

    feat: 微前端改造

---
+++ formily/antd/playground/widgets/XtProvider.tsx
@@ -0,0 +1,27 @@
+/**
+ *  eslint-disable react/display-name
+ *
+ * @format
+ */
+/* eslint-disable */
+
+import React from 'react'
+import { ConfigProvider as ConfigProvider0 } from 'antd'
+import zhCN from 'antd/lib/locale-provider/zh_CN'
+import { ConfigProvider as XTConfigProvider0 } from 'xt-design/es/ConfigProvider'
+
+const ConfigProvider = ConfigProvider0 as any
+const XTConfigProvider = XTConfigProvider0 as any
+
+const getPopupContainer = (node: { parentNode: any }) => {
+  const targets = document.querySelectorAll(`[data-qiankun="designable"]`)
+  return targets?.[0] || document.body
+}
+
+export const XtProvider = ({ children }) => (
+  <ConfigProvider locale={zhCN} getPopupContainer={getPopupContainer}>
+    <XTConfigProvider getPopupContainer={getPopupContainer}>
+      {children}
+    </XTConfigProvider>
+  </ConfigProvider>
+)
